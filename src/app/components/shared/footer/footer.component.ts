b87ed8c0c0988e8aa60da1fcdc1367d1384dabd8
Julia Langhammer <julia.langhammer@mni.thm.de>
2019-09-10 13:01:57 +0200

    Make help- and imprint-page accessible by tabulator, first try at cookie page

--- src/app/components/shared/footer/footer.component.ts
+++ src/app/components/shared/footer/footer.component.ts
@@ -74,7 +74,7 @@ showDemo() {
 showCookieModal() {
   const dialogRef = this.dialog.open(CookiesComponent, {
     width: '60%',
-    autoFocus: false
+    autoFocus: true
   });
   dialogRef.disableClose = true;
   dialogRef.componentInstance.deviceType = this.deviceType;
