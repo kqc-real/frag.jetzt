793c6cd1fbf9643f1c05efd03fbfaf46ef0022af
Marco Herzog <marco.herzog@mni.thm.de>
2019-09-10 16:07:35 +0200

    Add dialog-component to help-page

--- src/app/components/home/_dialogs/cookies/cookies.component.ts
+++ src/app/components/home/_dialogs/cookies/cookies.component.ts
@@ -1,18 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { DataProtectionComponent } from '../data-protection/data-protection.component';
-import { MatDialog } from '@angular/material';
+import { MatDialog, MatDialogRef } from '@angular/material';
+import { DialogConfirmActionButtonType } from '../../../shared/dialog/dialog-action-buttons/dialog-action-buttons.component';
 
 @Component({
   selector: 'app-cookies',
   templateUrl: './cookies.component.html',
   styleUrls: ['./cookies.component.scss']
 })
-export class CookiesComponent implements OnInit {
+export class CookiesComponent implements OnInit, AfterContentInit {
+
+  @ViewChild('header')
+  dialogTitle: ElementRef;
 
   deviceType: string;
   currentLang: string;
 
-  constructor(private dialog: MatDialog) { }
+  confirmButtonType: DialogConfirmActionButtonType = DialogConfirmActionButtonType.Primary;
+
+  constructor(private dialog: MatDialog, private dialogRef: MatDialogRef<CookiesComponent>) {
+  }
+
+  ngAfterContentInit() {
+    const elem: HTMLElement = this.dialogTitle.nativeElement;
+    elem.focus();
+  }
 
   ngOnInit() {
     this.currentLang = localStorage.getItem('currentLang');
@@ -20,11 +32,13 @@ export class CookiesComponent implements OnInit {
 
   acceptCookies() {
     localStorage.setItem('cookieAccepted', 'true');
+    this.dialogRef.close();
   }
 
   exitApp() {
     localStorage.setItem('cookieAccepted', 'false');
     // TODO somehow exit the app, since the user didn't accept cookie usage
+    this.dialogRef.close();
   }
 
   openDataProtection() {
@@ -32,5 +46,19 @@ export class CookiesComponent implements OnInit {
     width: '60%'
   });
   dialogRef.componentInstance.deviceType = this.deviceType;
-}
+  }
+
+  /**
+   * Returns a lambda which closes the dialog on call.
+   */
+  buildConfirmActionCallback(): () => void {
+    return () => this.acceptCookies();
+  }
+
+  /**
+   * Returns a lambda which closes the dialog on call.
+   */
+  buildDeclineActionCallback(): () => void {
+    return () => this.exitApp();
+  }
 }
