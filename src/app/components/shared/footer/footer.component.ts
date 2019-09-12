d16b767a0300346eb8503cd3773de26d429ca6c5
Julia Langhammer <julia.langhammer@mni.thm.de>
2019-09-12 11:24:18 +0200

    update demo dialog

--- src/app/components/shared/footer/footer.component.ts
+++ src/app/components/shared/footer/footer.component.ts
@@ -57,58 +57,51 @@ export class FooterComponent implements OnInit {
     }
   }
 
-showDemo() {
-  const dialogRef = this.dialog.open(DemoVideoComponent, {
-    position: {
-      left: '10px',
-      right: '10px'
-    },
-    maxWidth: '100vw',
-    maxHeight: '100vh',
-    height: '100%',
-    width: '100%'
-  });
-  dialogRef.componentInstance.deviceType = this.deviceType;
-}
+  showDemo() {
+    const dialogRef = this.dialog.open(DemoVideoComponent, {
+      width: '70%'
+    });
+    dialogRef.componentInstance.deviceType = this.deviceType;
+  }
 
-showCookieModal() {
-  const dialogRef = this.dialog.open(CookiesComponent, {
-    width: '70%',
-    autoFocus: true,
-  });
-  dialogRef.disableClose = true;
-  dialogRef.componentInstance.deviceType = this.deviceType;
-}
+  showCookieModal() {
+    const dialogRef = this.dialog.open(CookiesComponent, {
+      width: '70%',
+      autoFocus: true,
+    });
+    dialogRef.disableClose = true;
+    dialogRef.componentInstance.deviceType = this.deviceType;
+  }
 
-showImprint() {
-  const dialogRef = this.dialog.open(ImprintComponent, {
-    width: '40%'
-  });
-  dialogRef.componentInstance.deviceType = this.deviceType;
-}
+  showImprint() {
+    const dialogRef = this.dialog.open(ImprintComponent, {
+      width: '40%'
+    });
+    dialogRef.componentInstance.deviceType = this.deviceType;
+  }
 
-showHelp() {
-  const dialogRef = this.dialog.open(HelpPageComponent, {
-    width: '60%'
-  });
-  dialogRef.componentInstance.deviceType = this.deviceType;
-}
+  showHelp() {
+    const dialogRef = this.dialog.open(HelpPageComponent, {
+      width: '60%'
+    });
+    dialogRef.componentInstance.deviceType = this.deviceType;
+  }
 
-showDataProtection() {
-  const dialogRef = this.dialog.open(DataProtectionComponent, {
-    width: '60%'
-  });
-  dialogRef.componentInstance.deviceType = this.deviceType;
-}
+  showDataProtection() {
+    const dialogRef = this.dialog.open(DataProtectionComponent, {
+      width: '60%'
+    });
+    dialogRef.componentInstance.deviceType = this.deviceType;
+  }
 
-useLanguage(language: string) {
-  this.translateService.use(language);
-  localStorage.setItem('currentLang', language);
-  this.langService.langEmitter.emit(language);
-}
+  useLanguage(language: string) {
+    this.translateService.use(language);
+    localStorage.setItem('currentLang', language);
+    this.langService.langEmitter.emit(language);
+  }
 
-changeTheme(theme: Theme) {
-  this.themeClass = theme.name;
-  this.themeService.activate(theme.name);
-}
+  changeTheme(theme: Theme) {
+    this.themeClass = theme.name;
+    this.themeService.activate(theme.name);
+  }
 }
