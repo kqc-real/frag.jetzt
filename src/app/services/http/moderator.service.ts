8c1ada5c8ea1b73678c7b6d6c546018fabb77d07
Lukas Mauß <lukas.mauss@mni.thm.de>
2019-10-08 20:51:22 +0200

    Add room to moderators room-list after adding to moderators

--- src/app/services/http/moderator.service.ts
+++ src/app/services/http/moderator.service.ts
@@ -19,6 +19,7 @@ export class ModeratorService extends BaseHttpService {
     user: '/user',
     find: '/find'
   };
+  private joinDate = new Date(Date.now());
 
   constructor(private http: HttpClient) {
     super();
@@ -65,5 +66,10 @@ export class ModeratorService extends BaseHttpService {
       tap(() => ''),
       catchError(this.handleError('getUserData', []))
     );
+  }
+
+  addToHistory(roomId: string, userId: string): void {
+    const connectionUrl = `${ this.apiUrl.base + this.apiUrl.user }/${ userId }/roomHistory`;
+    this.http.post(connectionUrl, { roomId: roomId, lastVisit: this.joinDate.getTime() }, httpOptions).subscribe(() => {});
   }
 }
